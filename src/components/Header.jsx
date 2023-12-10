f725c6b4edb782af42880ac56e4af14539af7657
thuyvy76207 <1951052237vi@ou.edu.vn>
2023-12-10 15:23:44 +0700

    Navitem header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,41 @@
 import React from 'react';
 import { images } from '../constants';
 
+const navItemInfo = [
+  { name: 'Home' },
+  { name: 'Articles' },
+  { name: 'Page' },
+  { name: 'Pricing' },
+  { name: 'Fag' },
+];
+
+const NavItem = ({ name }) => {
+  return (
+    <li className=" relative group">
+      <a href="/" className="px-4 py-2 ">
+        {name}
+      </a>
+      <span className="text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">
+        /
+      </span>
+    </li>
+  );
+};
+
 const Header = () => {
   return (
     <section>
-      <header className="container mx-auto px-5 flex justify-between py-4">
+      <header className="container mx-auto px-5 flex justify-between py-4 items-center">
         <div>
           <img src={images.Logo} alt="logo"></img>
         </div>
-        <div className="flex gap-x-9">
-          <ul className="flex gap-x-5">
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/">Articles</a>
-            </li>
-            <li>
-              <a href="/">Page</a>
-            </li>
-            <li>
-              <a href="/">Pricing</a>
-            </li>
-            <li>
-              <a href="/">Fag</a>
-            </li>
+        <div className="flex gap-x-9 items-center">
+          <ul className="flex gap-x-2 font-semibold">
+            {navItemInfo.map((item) => {
+              return <NavItem key={item.name} name={item.name} />;
+            })}
           </ul>
-          <button className="border-2 border-blue-500 px-6 py-2">
+          <button className="border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300">
             Sign in
           </button>
         </div>
