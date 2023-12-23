2c5ab3bb4953c55abeaa22f7748adc71f5c0f38d
thuyvy76207 <1951052237vi@ou.edu.vn>
2023-12-23 09:46:23 +0700

    CTA

--- src/pages/home/container/CTA.jsx
+++ src/pages/home/container/CTA.jsx
@@ -4,7 +4,7 @@ const CTA = () => {
   return (
     <>
       <svg
-        className="w-full h-auto max-h-40"
+        className="w-full h-auto max-h-40 translate-y-[1px]"
         preserveAspectRatio="none"
         viewBox="0 0 2160 263"
         fill="none"
@@ -18,7 +18,25 @@ const CTA = () => {
           fill="#0D2436"
         />
       </svg>
-      <section></section>
+      <section className="relative bg-dark-hard px-5">
+        <div className="container grid grid-cols-12 mx-auto">
+          <div className="col-span-12">
+            <h2 className="text-white font-roboto font-bold text-2xl">
+              Get out stories delivered From us to your inbox
+            </h2>
+            <div className="w-full max-w-[494px] mt-12 space-y-3 mx-auto">
+              <input
+                type="text"
+                className="px-4 py-3 rounded-lg w-full placeholder:text-dark-light"
+                placeholder="Your Email"
+              />
+              <button className="px-4 py-3 rounded-lg bg-primary text-white w-full font-bold">
+                Get started
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
