2b67c2b5285ed5aea0dfdfe888b55698f24b7b7f
thuyvy76207 <1951052237vi@ou.edu.vn>
2024-01-04 09:29:07 +0700

    Complete footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { images } from '../constants';
+import {
+  FaTwitter,
+  FaFacebook,
+  FaYoutube,
+  FaTelegram,
+  FaHeart,
+} from 'react-icons/fa';
+import { AiFillInstagram } from 'react-icons/ai';
 
 const Footer = () => {
   return (
     <section className="bg-dark-hard">
-      <footer className="container mx-auto grid grid-cols-10">
-        <div className="col-span-5">
-          <h3 className="text-dark-light font-bold">Product</h3>
-          <ul className="text-[#959EAD] text-sm mt-5 space-y-4">
+      <footer className="container mx-auto grid grid-cols-10 px-5 py-10 gap-y-10 gap-x-5 md:pt-20 md:grid-cols-12 lg:grid-cols-10 lg:gap-x-10">
+        <div className="col-span-5 md:col-span-4 lg:col-span-2">
+          <h3 className="text-dark-light font-bold md:text-lg">Product</h3>
+          <ul className="text-[#959EAD] text-sm mt-5 space-y-4 md:text-base">
             <li>
               <a href="/">Landingpage</a>
             </li>
@@ -24,9 +33,9 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <div className="col-span-5">
-          <h3 className="text-dark-light font-bold">Product</h3>
-          <ul className="text-[#959EAD] text-sm mt-5 space-y-4">
+        <div className="col-span-5 md:col-span-4 lg:col-span-2">
+          <h3 className="text-dark-light font-bold md:text-lg">Product</h3>
+          <ul className="text-[#959EAD] text-sm mt-5 space-y-4 md:text-base">
             <li>
               <a href="/">Landingpage</a>
             </li>
@@ -44,9 +53,9 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <div className="col-span-5">
-          <h3 className="text-dark-light font-bold">Product</h3>
-          <ul className="text-[#959EAD] text-sm mt-5 space-y-4">
+        <div className="col-span-5 md:col-span-4 md:col-start-5 lg:col-span-2 lg:col-start-auto">
+          <h3 className="text-dark-light font-bold md:text-lg">Product</h3>
+          <ul className="text-[#959EAD] text-sm mt-5 space-y-4 md:text-base">
             <li>
               <a href="/">Landingpage</a>
             </li>
@@ -64,9 +73,9 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <div className="col-span-5">
-          <h3 className="text-dark-light font-bold">Product</h3>
-          <ul className="text-[#959EAD] text-sm mt-5 space-y-4">
+        <div className="col-span-5 md:col-span-4 lg:col-span-2">
+          <h3 className="text-dark-light font-bold md:text-lg">Product</h3>
+          <ul className="text-[#959EAD] text-sm mt-5 space-y-4 md:text-base">
             <li>
               <a href="/">Landingpage</a>
             </li>
@@ -83,6 +92,52 @@ const Footer = () => {
               <a href="/">Pricing</a>
             </li>
           </ul>
+        </div>
+        <div className="col-span-10 md:order-first md:col-span-4 lg:col-span-2">
+          <img
+            src={images.Logo}
+            alt="logo"
+            className="brightness-0 invert mx-auto md:mx-0"
+          />
+          <p className="text-sm text-dark-light text-center mt-4 md:text-left md:text-base lg:text-sm">
+            Build a modern and creative website with crealand
+          </p>
+          <ul className="flex justify-center items-center mt-5 space-x-4 text-gray-300 md:justify-start">
+            <li>
+              <a href="/">
+                <FaTwitter className="w-6 h-auto" />
+              </a>
+            </li>
+            <li>
+              <a href="/">
+                <FaYoutube className="w-6 h-auto" />
+              </a>
+            </li>
+            <li>
+              <a href="/">
+                <AiFillInstagram className="w-6 h-auto" />
+              </a>
+            </li>
+            <li>
+              <a href="/">
+                <FaFacebook className="w-6 h-auto" />
+              </a>
+            </li>
+            <li>
+              <a href="/">
+                <FaTelegram className="w-6 h-auto" />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="hidden md:flex flex-col items-center space-y-4 md:col-span-12 lg:col-span-10">
+          <div className="bg-primary text-white p-3 rounded-full">
+            {' '}
+            <FaHeart className="w-7 h-auto" />
+          </div>
+          <p className="font-bold italic text-dark-light">
+            Copyright © 2024. Moonfo with love.
+          </p>
         </div>
       </footer>
     </section>
