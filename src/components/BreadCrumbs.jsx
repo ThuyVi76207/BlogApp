1596928b382c4b80fefe793a69743bafd9c38285
thuyvy76207 <1951052237vi@ou.edu.vn>
2024-01-12 09:22:16 +0700

    Complete article details page

--- src/components/BreadCrumbs.jsx
+++ src/components/BreadCrumbs.jsx
@@ -6,7 +6,10 @@ const BreadCrumbs = ({ data }) => {
     <div className="flex items-center py-4 overflow-x-auto whitespace-nowrap">
       {data.map((item, index) => {
         return (
-          <div className="text-black opacity-50 text-xs" key={index}>
+          <div
+            className="text-black opacity-50 text-xs font-roboto md:text-sm"
+            key={index}
+          >
             <Link to={item.link}>{item.name}</Link>
             {index !== data.length - 1 && <span className="px-3">/</span>}
           </div>
