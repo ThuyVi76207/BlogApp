888caaa23ebf293ab43113745992d91e79fecf58
thuyvy76207 <1951052237vi@ou.edu.vn>
2024-01-12 15:51:29 +0700

    Create comment user

--- src/components/Comments/CommentsContainer.jsx
+++ src/components/Comments/CommentsContainer.jsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CommentForm from './CommentForm';
+import { getCommentsData } from '../../data/comment';
 
 const CommentsContainer = ({ className }) => {
+  const [comments, setComments] = useState([]);
+
+  console.log('Check comment', comments);
+
+  useEffect(() => {
+    (async () => {
+      const commentData = await getCommentsData();
+      setComments(commentData);
+    })();
+  }, []);
+
+  const addCommentHandler = (value, parent = null, replyOnUser = null) => {
+    const newComment = {
+      _id: '10',
+      user: { _id: 'a', name: 'Mohammad Rezaill' },
+      desc: value,
+      post: '1',
+      parent: parent,
+      replyOnUser: replyOnUser,
+      createAt: '2023-12-31T17:22:05.092+0000',
+    };
+    setComments((curState) => {
+      return [newComment, ...curState];
+    });
+  };
+
   return (
     <div className={`${className}`}>
-      <CommentForm btnLabel={'Send'} />
+      <CommentForm
+        btnLabel={'Send'}
+        formSubmitHandler={(value) => addCommentHandler(value)}
+      />
     </div>
   );
 };
