8a936b1690073fb81c612c958a4cb5ecd4a20251
thuyvy76207 <1951052237vi@ou.edu.vn>
2024-01-25 15:32:58 +0700

    Update social media

--- src/components/SocialShareButton.jsx
+++ src/components/SocialShareButton.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { FaSquareFacebook } from 'react-icons/fa6';
 
 const SocialShareButton = ({ url, title }) => {
   return (
-    <div>
-      <a href="/"></a>
+    <div className="w-full flex justify-between">
+      <a href="/" target="_blank" ref="no"></a>
+      <FaSquareFacebook />
     </div>
   );
 };
